1f2678f95f5519f4ce7e55d6c146de433dc33986
fatlard1993 <fatlard1993@gmail.com>
2019-03-20 23:53:19 -0700

    fix folder

--- server/index.js
+++ server/index.js
@@ -37,9 +37,9 @@ app.get('/test', function(req, res){
 	res.send('test');
 });
 
-app.use('/resources', staticServer(path.join(__dirname, '../client/resources')));
+app.use('/resources', staticServer(path.join(rootFolder, '../client/resources')));
 
-app.use('/fonts', staticServer(path.join(__dirname, '../client/fonts')));
+app.use('/fonts', staticServer(path.join(rootFolder, '../client/fonts')));
 
 app.get('/home', sendPage('index'));
 
